f9c9b5af6b5ddec49049e06343cbb3583a009d63
unknown <sunantha1881@gmail.com>
2020-01-24 16:16:12 +0700

    lab8

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -2,8 +2,19 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
 
+
+int main(){	
+	
+    double loan,rate,pay,PrevBalance,payment,NewBalance=1,Interest,Total,Balance=0;
+	cout<<"Enter initial loan: ";
+	cin>>loan;
+	cout<<"Enter interet rate per year (%): ";
+	cin>>rate;
+    cout<<"Enter amount you can pay per year: ";
+	cin>>pay;
+
+    
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -15,16 +26,38 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+
+	
+	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	
+	
+	NewBalance=loan;
+    for(int x=1; x<=1000 ;x++){
+	
+	Balance=NewBalance;	
+	Interest=Balance*(rate*0.01);
+	Total=Balance+Interest;
+	payment=pay;
+	NewBalance=Total-payment;
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << x; 
+	cout << setw(13) << left <<Balance;
+	cout << setw(13) << left <<Interest;
+	cout << setw(13) << left <<Total;
+	if(Total<pay){
+		payment=Total;
+		NewBalance=Total-payment;
+	cout << setw(13) << left <<payment;
+	cout << setw(13) << left <<NewBalance;
+	}else{
+	cout << setw(13) << left <<payment;
+	cout << setw(13) << left <<NewBalance;
 	cout << "\n";	
+	}
+	if(NewBalance==0) break;
+	}
 	
 	return 0;
 }
--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 //Write the function printO() here
+
+void printO(int x,int y){
+	if(x<=0 || y<=0)
+    cout<<"Invalid input";
+
+	for(int i=0 ; i < x ;i++ ){
+		for(int j = 0; j < y; j++ ){
+			cout << 'O';
+		}
+		cout<<'\n' ; 
+		
+	}
+	
+	}
+ 
 
 int main(){
 	
@@ -17,7 +33,7 @@ int main(){
 	printO(0,3);
 	cout << "\n";	
 	
-	printO(7,-1);
+	printO(7,0);
 	cout << "\n";	
 	
 	return 0;
